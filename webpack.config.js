a240c73e271e2f7d1628fa4fbf38c101c9007df0
Swyzsh <swyzsh@protonmail.com>
2024-06-04 20:24:48 +0530

    init

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    index: './src/index.js',
+  },
+  devtool: 'source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      favicon: './src/metadata/favicon.ico',
+      inject: 'body',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/',
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|webp|mp3|mp4)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  performance: {
+    hints: false,
+  },
+};
