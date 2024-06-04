1dd66a7bd142e7d8bc5b7f5d10eaf28bd5b1bb73
Swyzsh <swyzsh@protonmail.com>
2024-06-04 20:43:04 +0530

    init index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import './style.css';
+
+let currentLocation = null;
+let temp_C = 0;
+let temp_F = 0;
+
+const locationDisplay = document.createElement('div');
+
+
+document.body.appendChild(locationDisplay);
+
+const tempDisplay = document.createElement('div');
+
+
+document.body.appendChild(tempDisplay);
+
+const dayForcast = document.createElement('div');
+
+
+document.body.appendChild(dayForcast);
+
+const pastThreeForcast = document.createElement('div');
+
+
+document.body.appendChild(pastThreeForcast);
+
+document.addEventListener('DOMContentLoaded', function() {
+
+});
